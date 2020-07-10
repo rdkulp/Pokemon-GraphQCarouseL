45a3fadf818114e5f6df728569831dbdcd314a4a
rdkulp <rdkulp@gmail.com>
2020-07-10 17:55:09 -0600

    Initial Setup

---
+++ components/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+  <img class="logo" src="~/assets/logo.png" />
+</template>
+
+<style>
+.logo {
+  animation: 1s appear;
+  margin: auto;
+  margin-top: 1vh;
+  width: 33.33%;
+  align-items: center;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
+
